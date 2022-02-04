937516a165d714c4502e0a18219f309ba7b7e937
HellyVora <hellyvora2001@gmail.com>
2022-02-04 23:23:46 +0530

    login, signup done

--- HelperLand/HelperLand/Controllers/HomeController.cs
+++ HelperLand/HelperLand/Controllers/HomeController.cs
@@ -6,20 +6,29 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using HelperLand.Data;
+using HelperLand.Models;
 
 namespace HelperLand.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly HelperLandContext _helperLandContext=new HelperLandContext();
+
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
+        //public HomeController(HelperLandContext helperLandContext)
+        //{
+        //    _helperLandContext = helperLandContext;
+        //}
 
         public IActionResult Index()
         {
+            User user = new User();
             return View();
         }
 
@@ -41,5 +50,80 @@ namespace HelperLand.Controllers
         public ViewResult About() => View();
 
         public ViewResult Faq() => View();
+
+        public ViewResult BecomeAPro()
+        {
+            User user = new User();
+            return View(user);
+        }
+        public IActionResult SignupPage()
+        {
+            User user = new User();
+            return View(user);
+        }
+
+        [HttpPost]
+
+        public IActionResult SignupPage(User user)
+        {
+            if(_helperLandContext.Users.Where(x => x.Email == user.Email).Count()==0 && _helperLandContext.Users.Where(x => x.Mobile == user.Mobile).Count() == 0)
+            {
+                user.UserTypeId = 1;
+                user.CreatedDate = DateTime.Now;
+                user.ModifiedDate = DateTime.Now;
+                user.IsRegisteredUser = true;
+                user.ModifiedBy = 123;
+
+                _helperLandContext.Users.Add(user);
+                _helperLandContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.message = "User already exists";
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult BecomeAPro(User user)
+        {
+            if (_helperLandContext.Users.Where(x => x.Email == user.Email).Count() == 0 && _helperLandContext.Users.Where(x => x.Mobile == user.Mobile).Count() == 0)
+            {
+                user.UserTypeId = 2;
+                user.CreatedDate = DateTime.Now;
+                user.ModifiedDate = DateTime.Now;
+                user.IsRegisteredUser = true;
+                user.ModifiedBy = 123;
+
+                _helperLandContext.Users.Add(user);
+                _helperLandContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.message = "User already exists";
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(User user)
+        {
+            if (_helperLandContext.Users.Where(x => x.Email == user.Email).Count() == 0)
+            {
+                if ( _helperLandContext.Users.Where(x => x.Password == user.Password).Count() == 0)
+                         ViewBag.message = "enter correct  password";
+            }
+            else
+            {
+                ViewBag.message = "Login Successfull";
+                return RedirectToAction("ContactUs");
+            }
+                return View();
+            
+
+        }
+            
+        
     }
 }
