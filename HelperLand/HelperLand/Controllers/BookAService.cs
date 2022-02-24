5a29496655a1c31b958216fc029afea80d28c607
HellyVora <hellyvora2001@gmail.com>
2022-02-24 10:55:18 +0530

    book_now

---
+++ HelperLand/HelperLand/Controllers/BookAService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelperLand.Models;
+using HelperLand.Data;
+using HelperLand.Controllers;
+
+namespace HelperLand.Controllers
+{
+    public class BookAService : Controller
+    {
+        private readonly HelperLandContext _helperLandContext = new HelperLandContext();
+        public IActionResult Index()
+        {
+            Zipcode zip = new Zipcode();
+            return View();
+        }
+    }
+}
